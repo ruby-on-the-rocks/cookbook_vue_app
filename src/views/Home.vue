afa5cbb27b72477c9e6ba6e2188c0e7bf617cdfb
Peter Jang <peter.x.jang@gmail.com>
2019-03-31 11:48:28 -0500

    Add new/create recipe (using hard coded values)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <p>My name is {{ name }}!</p>
+    <h1>New Recipe</h1>
+    <button v-on:click="createRecipe()">Create recipe</button>
     <div v-for="recipe in recipes">
       <h2>Title: {{ recipe.title }}</h2>
       <img v-bind:src="recipe.image_url" v-bind:alt="recipe.title" />
@@ -38,6 +40,20 @@ export default {
       console.log(this.recipes);
     });
   },
-  methods: {}
+  methods: {
+    createRecipe: function() {
+      console.log("Create the recipe...");
+      var params = {
+        body_title: "example title",
+        body_chef: "example chef",
+        body_prep_time: 100,
+        body_ingredients: "example ingredients",
+        body_directions: "example directions"
+      };
+      axios.post("/api/recipes", params).then(response => {
+        console.log("Success", response.data);
+      });
+    }
+  }
 };
 </script>
