4ce143f705cbfb56ad88b0cbea7467ea2864fbd0
Peter Jang <peter.x.jang@gmail.com>
2019-04-01 19:46:14 -0500

    Add edit/update recipe action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,6 +28,22 @@
         <p>Prep time: {{ recipe.prep_time }}</p>
         <p>Ingredients: {{ recipe.ingredients }}</p>
         <p>Directions: {{ recipe.directions }}</p>
+        <div>
+          <h4>Edit recipe</h4>
+          Title:
+          <input type="text" v-model="recipe.title" />
+          Chef:
+          <input type="text" v-model="recipe.chef" />
+          Prep time:
+          <input type="text" v-model="recipe.prep_time" />
+          Ingredients:
+          <input type="text" v-model="recipe.ingredients" />
+          Directions:
+          <input type="text" v-model="recipe.directions" />
+          Image url:
+          <input type="text" v-model="recipe.image_url" />
+          <button v-on:click="updateRecipe(recipe)">Update recipe</button>
+        </div>
       </div>
     </div>
   </div>
@@ -87,6 +103,18 @@ export default {
       } else {
         this.currentRecipe = recipe;
       }
+    },
+    updateRecipe: function(recipe) {
+      var params = {
+        body_title: recipe.title,
+        body_chef: recipe.chef,
+        body_ingredients: recipe.ingredients,
+        body_directions: recipe.directions
+      };
+      axios.patch("/api/recipes/" + recipe.id, params).then(response => {
+        console.log("Successfully updated", response.data);
+        recipe = response.data;
+      });
     }
   }
 };
